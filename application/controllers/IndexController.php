172a9bb75c1827fb68db5219964bb3c6f105be1c
domeix <domeix@web.de>
2015-08-21 14:25:29 +0200

    aufgeräumt und so :D

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,14 +1,18 @@
-<?php 
-class IndexController extends Zend_Controller_Action {
-	
-	public function init() {
-		
-	}
-	
-	public function indexAction() {
-		
-	}
-	
-	
-	
+<?php
+
+class IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+
 }
+
